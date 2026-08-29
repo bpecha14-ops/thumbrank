69af16dd6fe33f7f4c13153f861124c40785b13d
bpecha14-ops <bpecha14@gmail.com>
2026-08-29 21:56:22 +0300

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,10 +1,8 @@
 import { createBrowserClient } from '@supabase/ssr'
 
-const SUPABASE_URL = 'https://igdswmsdtbaqvlycucum.supabase.co'
-
 export function createClient() {
   return createBrowserClient(
-    SUPABASE_URL,
+    process.env.SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
