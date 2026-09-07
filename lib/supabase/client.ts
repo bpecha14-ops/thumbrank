61b0b1ebf3517a846d3e33bcc246fd87050fb717
bpecha14-ops <bpecha14@gmail.com>
2026-09-08 02:57:15 +0300

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-let client: any = null;
-
 export function getSupabaseClient() {
-  if (client) return client;
-  
   const url = 'https://igdswmsdtbaqvlycucum.supabase.co';
   const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
   
@@ -13,6 +9,5 @@ export function getSupabaseClient() {
     return null;
   }
   
-  client = createClient(url, key);
-  return client;
+  return createClient(url, key);
 }
